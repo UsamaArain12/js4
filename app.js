ad2ee4acbf416a80445d5ae45861fcf92f198056
Usama Arain <ua339716@gmail.com>
2023-10-07 23:09:35 -0700

    js4

---
+++ app.js
@@ -0,0 +1,19 @@
+var age = 25
+
+alert("I am " + age + " years old")
+
+var N =14;
+
+var firstMessage = "you have visited this site ";
+var secondMessage=" times"
+
+alert(firstMessage+N+secondMessage);
+
+var birthYear = ("My Birth Year is 1998")
+document.write (birthYear)
+
+var userName = "<b>Jhone Doe</b> ";
+var productQuantity = 5;
+var productTitle = "<b> T-Shirts</b>";
+
+document.write("<br><br><br>"+userName+"orderd "+"<b>"+productQuantity+"</b>"+productTitle+" on XYZ Clothing Store");
